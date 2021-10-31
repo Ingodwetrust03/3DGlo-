d941be63c7fff1ff22f84600e65b6bff1536bd63
svetlana18055 <svetlana18055@yandex.ru>
2021-10-31 22:03:15 +0300

    Урок 21

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@
 import timer from './modules/timer';
 import menu from './modules/menu';
 import modal from './modules/modal';
+import validator from './modules/validator';
 
 
 timer('02 november 2021')
 menu()
 modal()
+validator()
