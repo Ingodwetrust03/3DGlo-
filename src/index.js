c56933c99132639ad3fcf78db1f6fcb9c5f0a92c
svetlana18055 <svetlana18055@yandex.ru>
2021-11-04 18:17:56 +0300

    исправила отступы, убрала скрипт плавного скролла - не работает

--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@
 import timer from './modules/timer';
 import menu from './modules/menu';
 import modal from './modules/modal';
-import validator from './modules/validator';
-import smoothScroll from './modules/smoothScroll';
-import tabs from './modules/tabs';
+import slider from './modules/slider';
 
 
-timer('10 november 2021')
+timer('24 november 2021')
 menu()
 modal()
-validator()
-smoothScroll()
-tabs()
+slider()
+
--- src/modules/tabs.js
+++ src/modules/tabs.js
@@ -7,21 +7,22 @@ const tabs = () => {
     const tabContent = document.querySelectorAll('.service-tab')
 
     tabPanel.addEventListener('click', (e) => {
-        if ( e.target.closest('.service-header-tab')){
-            const tabBtnClick = e.target.closest('.service-header-tab')
-            tabBtns.forEach((btn, index) => {
-            if(btn === tabBtnClick){
+        if (!e.target.closest('.service-header-tab')) {
+            return
+        }
+        const tabBtnClick = e.target.closest('.service-header-tab')
+        tabBtns.forEach((btn, index) => {
+            if (btn === tabBtnClick) {
                 btn.classList.add('active')
                 tabContent[index].classList.remove('d-none')
             } else {
                 btn.classList.remove('active')
                 tabContent[index].classList.add('d-none')
-                }
-            })
-        }
+            }
+        })
     })
 
-    
+
 }
 
 export default tabs
